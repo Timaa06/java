32432fb0ce3b0a14308768876a6a4e1e2ceed2de
Fatoumata dite Tima Sidibé <sidibefatoumataditetima@gmail.com>
2025-02-11 16:29:53 +0100

    MAJ

---
+++ EXO1/Main.java
@@ -0,0 +1,12 @@
+package EXO1;
+
+public class Main {
+    public static void main(String[] args) {
+        Voiture voiture = new Voiture();
+        voiture.couleur = "Rouge";
+        voiture.marque = "Toyota";
+        voiture.modele = "Rav4";
+
+        voiture.demarrer();
+    }
+}
---
+++ EXO1/Voiture.java
@@ -0,0 +1,12 @@
+package EXO1;
+
+public class Voiture {
+    public String marque;
+    public String modele;
+    public String couleur;
+
+    public void demarrer () {
+        System.out.println("La voiture a demarré !");
+    }
+    
+}
